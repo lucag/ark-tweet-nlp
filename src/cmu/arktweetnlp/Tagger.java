8d8cab5b98d8fde900166952db77b11ce2cf053b
Luca Gilardi <lucag@icsi.berkeley.edu>
2016-02-20 14:15:18 -0800

    Add models.

--- src/cmu/arktweetnlp/Tagger.java
+++ src/cmu/arktweetnlp/Tagger.java
@@ -1,99 +1,96 @@
 package cmu.arktweetnlp;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
 import cmu.arktweetnlp.impl.Model;
 import cmu.arktweetnlp.impl.ModelSentence;
 import cmu.arktweetnlp.impl.Sentence;
 import cmu.arktweetnlp.impl.features.FeatureExtractor;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Tagger object -- wraps up the entire tagger for easy usage from Java.
- * 
+ * <p>
  * To use:
- * 
+ * <p>
  * (1) call loadModel().
- * 
+ * <p>
  * (2) call tokenizeAndTag() for every tweet.
- *  
+ * <p>
  * See main() for example code.
- * 
+ * <p>
  * (Note RunTagger.java has a more sophisticated runner.
  * This class is intended to be easiest to use in other applications.)
  */
 public class Tagger {
-	public Model model;
-	public FeatureExtractor featureExtractor;
+    public Model model;
+    public FeatureExtractor featureExtractor;
 
-	/**
-	 * Loads a model from a file.  The tagger should be ready to tag after calling this.
-	 * 
-	 * @param modelFilename
-	 * @throws IOException
-	 */
-	public void loadModel(String modelFilename) throws IOException {
-		model = Model.loadModelFromText(modelFilename);
-		featureExtractor = new FeatureExtractor(model, false);
-	}
+    /**
+     * Illustrate how to load and call the POS tagger.
+     * This main() is not intended for serious use; see RunTagger.java for that.
+     **/
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.out.println("Supply the model filename as first argument.");
+        }
+        String modelFilename = args[0];
 
-	/**
-	 * One token and its tag.
-	 **/
-	public static class TaggedToken {
-		public String token;
-		public String tag;
-	}
+        Tagger tagger = new Tagger();
+        tagger.loadModel(modelFilename);
 
+        String text = "RT @DjBlack_Pearl: wat muhfuckaz wearin 4 the lingerie party?????";
+        List<TaggedToken> taggedTokens = tagger.tokenizeAndTag(text);
 
-	/**
-	 * Run the tokenizer and tagger on one tweet's text.
-	 **/
-	public List<TaggedToken> tokenizeAndTag(String text) {
-		if (model == null) throw new RuntimeException("Must loadModel() first before tagging anything");
-		List<String> tokens = Twokenize.tokenizeRawTweetText(text);
+        for (TaggedToken token : taggedTokens) {
+            System.out.printf("%s\t%s\n", token.tag, token.token);
+        }
+    }
 
-		Sentence sentence = new Sentence();
-		sentence.tokens = tokens;
-		ModelSentence ms = new ModelSentence(sentence.T());
-		featureExtractor.computeFeatures(sentence, ms);
-		model.greedyDecode(ms, false);
+    /**
+     * Loads a model from a file.  The tagger should be ready to tag after calling this.
+     *
+     * @param modelFilename
+     * @throws IOException
+     */
+    public void loadModel(String modelFilename) throws IOException {
+        model = Model.loadModelFromText(modelFilename);
+        featureExtractor = new FeatureExtractor(model, false);
+    }
 
-		ArrayList<TaggedToken> taggedTokens = new ArrayList<TaggedToken>();
+    /**
+     * Run the tokenizer and tagger on one tweet's text.
+     **/
+    public List<TaggedToken> tokenizeAndTag(String text) {
+        if (model == null) throw new RuntimeException("Must loadModel() first before tagging anything");
+        List<String> tokens = Twokenize.tokenizeRawTweetText(text);
 
-		for (int t=0; t < sentence.T(); t++) {
-			TaggedToken tt = new TaggedToken();
-			tt.token = tokens.get(t);
-			tt.tag = model.labelVocab.name( ms.labels[t] );
-			taggedTokens.add(tt);
-		}
+        Sentence sentence = new Sentence();
+        sentence.tokens = tokens;
+        ModelSentence ms = new ModelSentence(sentence.T());
+        featureExtractor.computeFeatures(sentence, ms);
+        model.greedyDecode(ms, false);
 
-		return taggedTokens;
-	}
+        ArrayList<TaggedToken> taggedTokens = new ArrayList<TaggedToken>();
 
-	/**
-	 * Illustrate how to load and call the POS tagger.
-	 * This main() is not intended for serious use; see RunTagger.java for that.
-	 **/
-	public static void main(String[] args) throws IOException {
-		if (args.length < 1) {
-			System.out.println("Supply the model filename as first argument.");
-		}
-		String modelFilename = args[0];
+        for (int t = 0; t < sentence.T(); t++) {
+            TaggedToken tt = new TaggedToken();
+            tt.token = tokens.get(t);
+            tt.tag = model.labelVocab.name(ms.labels[t]);
+            taggedTokens.add(tt);
+        }
 
-		Tagger tagger = new Tagger();
-		tagger.loadModel(modelFilename);
+        return taggedTokens;
+    }
 
-		String text = "RT @DjBlack_Pearl: wat muhfuckaz wearin 4 the lingerie party?????";
-		List<TaggedToken> taggedTokens = tagger.tokenizeAndTag(text);
-
-		for (TaggedToken token : taggedTokens) {
-			System.out.printf("%s\t%s\n", token.tag, token.token);
-		}
-	}
+    /**
+     * One token and its tag.
+     **/
+    public static class TaggedToken {
+        public String token;
+        public String tag;
+    }
 
 }
--- twokenize.sh
+++ twokenize.sh
@@ -3,4 +3,9 @@
 # Only run the tokenizer.
 
 set -eu
-java -XX:ParallelGCThreads=2 -Xmx100m -jar $(dirname $0)/ark-tweet-nlp-0.3.2.jar --just-tokenize "$@"
+BASE=$(dirname $0)/ark-tweet-nlp/target
+VERSION="0.3.3"
+
+java -XX:ParallelGCThreads=2 -Xmx100m \
+     -jar ${BASE}/ark-tweet-nlp-${VERSION}-with-dependencies.jar \
+     --just-tokenize "$@"
